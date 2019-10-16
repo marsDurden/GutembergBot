cb6fb414e10b5bcfaadf68ae2530c78ebae89d30
Jacopo Schiavon <jacoposchiavon21@gmail.com>
2019-10-16 13:12:12 +0200

    updated json_writer with standard for comitato_autogestione.xlsx

--- json_writer.py
+++ json_writer.py
@@ -12,6 +12,14 @@ file_output = join(folder, "matricole.json")
 true_list = ['si', 'Si', 'sì', 'Sì']
 false_list = ['no', 'No']
 
+column_names = ['CognomeNome',
+                'Cellulare',
+                'Matricola',
+                'Mail',
+                'idTelegram',
+                'corsoBase',
+                'corsoBasso',
+                'note']
 
 def corsoEnc(r):
     if r['corsoBase']:
@@ -32,11 +40,12 @@ def path_check(f):
 
 def parse_xlsx(f):
     try:
-        df = pd.read_excel(f, header=0, true_values=true_list, false_values=false_list, skiprows=[0])
+        df = pd.read_excel(f, header=0, true_values=true_list, false_values=false_list, skiprows=[0,1], names=column_names)
     except:
-        df = pd.read_excel(f, header=0, skiprows=[0])
+        df = pd.read_excel(f, header=0, skiprows=[0,1], names=column_names)
         df['corsoBase'] = df['corsoBase'].apply(lambda x: True if x in true_list else False)
         df['corsoBasso'] = df['corsoBasso'].apply(lambda x: True if x in true_list else False)
+    print(df.head())
     return df
 
 
@@ -44,10 +53,10 @@ def main():
     path_check(file_input)
     dataset = parse_xlsx(file_input)
 
-    preformatted = {str(row['idTelegram']) + corsoEnc(row): {
-        'nome': row['Cognome e nome'],
+    preformatted = {str(int(row['idTelegram'])) + corsoEnc(row): {
+        'nome': row['CognomeNome'],
         'matricola': str(row['Matricola'])
-        } for index, row in dataset.iterrows()}
+        } for index, row in dataset.iterrows() if row['idTelegram'] == row['idTelegram']}
 
     preformatted['descrizione'] = "Questo è il file con le matricole e i nomi di chi chiude l'Aula Pollaio per la generazione automatica del file. Questo file è automaticamente generato dallo script json_writer.py. Validare il file sul sito https://jsonformatter.curiousconcept.com/ prima di mandarlo al bot."
 
